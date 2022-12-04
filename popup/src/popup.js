d3347e0c35cf2b0e0a604c36befdc80ccaba0469
NaimKabir <kabir.naim@gmail.com>
2022-12-03 20:46:13 -0800

    fmt

--- popup/src/popup.js
+++ popup/src/popup.js
@@ -56,33 +56,33 @@ function extractLinksFromDynamicRules(dynamicRulesResult) {
 }
 
 function renderTrashCanIcon() {
-  const iconSvg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+  const iconSvg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
   const iconPath = document.createElementNS(
-    'http://www.w3.org/2000/svg',
-    'path'
+    "http://www.w3.org/2000/svg",
+    "path"
   );
 
-  iconSvg.setAttribute('height', '16');
-  iconSvg.setAttribute('width', '16');
-  iconSvg.setAttribute('fill', 'currentColor');
-  iconSvg.setAttribute('viewBox', '0 0 16 16');
-  iconSvg.classList.add('bi');
-  iconSvg.classList.add('bi-trash');
+  iconSvg.setAttribute("height", "16");
+  iconSvg.setAttribute("width", "16");
+  iconSvg.setAttribute("fill", "currentColor");
+  iconSvg.setAttribute("viewBox", "0 0 16 16");
+  iconSvg.classList.add("bi");
+  iconSvg.classList.add("bi-trash");
 
   iconPath.setAttribute(
-    'd',
-    'M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z'
+    "d",
+    "M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"
   );
 
   iconSvg.appendChild(iconPath);
 
   const iconPath2 = document.createElementNS(
-    'http://www.w3.org/2000/svg',
-    'path'
+    "http://www.w3.org/2000/svg",
+    "path"
   );
-  iconPath2.setAttribute('fill-rule', 'evenodd')
+  iconPath2.setAttribute("fill-rule", "evenodd");
   iconPath2.setAttribute(
-    'd', 
+    "d",
     "M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"
   );
   iconSvg.appendChild(iconPath2);
@@ -93,12 +93,13 @@ function renderTrashCanIcon() {
 function renderLink(link) {
   const linksElement = document.getElementById(IDS.links);
   const linkNode = document.createElement("li");
-  linkNode.className = "list-group-item d-flex justify-content-between align-items-center list-group-item-action";
+  linkNode.className =
+    "list-group-item d-flex justify-content-between align-items-center list-group-item-action";
   const textNode = document.createTextNode(
     goPrefix + link.shortLink + " " + link.id
   );
   const buttonNode = document.createElement("button");
-  buttonNode.className = "btn btn-danger"
+  buttonNode.className = "btn btn-danger";
   buttonNode.appendChild(renderTrashCanIcon());
   buttonNode.addEventListener("click", () => {
     removeLink(link);
@@ -187,8 +188,8 @@ function prepopulateLongLinkForm(longLinkForm) {
 }
 
 function updateShortLinkHelp(shortLinkForm) {
-    const shortLinkHelp = document.getElementById("shortLinkHelp");
-    shortLinkHelp.innerHTML = "go/" + shortLinkForm.value;
+  const shortLinkHelp = document.getElementById("shortLinkHelp");
+  shortLinkHelp.innerHTML = "go/" + shortLinkForm.value;
 }
 function listenForShortLinkInputs(shortLinkForm) {
   shortLinkForm.addEventListener("keyup", () => {
