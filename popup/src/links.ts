ec8945f06cea90a3b584bbb74a32509151c475d1
NaimKabir <kabir.naim@gmail.com>
2022-12-05 03:55:38 -0800

    first cut caching

--- popup/src/links.ts
+++ popup/src/links.ts
@@ -2,6 +2,10 @@ import { PREFIX } from "./constants";
 
 const MAX_LINKS = 5; // chrome.declarativeNetRequest.MAX_NUMBER_OF_DYNAMIC_AND_SESSION_RULES;
 
+var INITIALIZED = false;
+var USED_IDS: {[key: string]: any} = {};
+var SHORTLINK_IDS: {[key: string]: any} = {};
+
 export interface Link {
   shortLink: string;
   longLink: string;
@@ -25,45 +29,80 @@ enum StorageType {
   ID_RESERVED = 2,
 }
 
-function storageKey(key: string, type: StorageType): string {
-  // We namespace bits of storage by prefixing access keys
-  let keyPrefix;
+function storageKey(type: StorageType) {
   switch (type) {
     case StorageType.SHORTLINK:
-      keyPrefix = "SL-";
-      break;
+      return "SHORTLINK_IDS";
     case StorageType.ID_RESERVED:
-      keyPrefix = "RS-";
-      break;
-    default:
-      keyPrefix = "";
+      return "USED_IDS";
   }
-  return keyPrefix + key;
+}
+
+export async function initStorage() {
+  // We do big fetches and then deal entirely with caches so we don't hit API rate limits
+  USED_IDS = await chrome.storage.local.get(storageKey(StorageType.ID_RESERVED));
+  SHORTLINK_IDS = await chrome.storage.local.get(storageKey(StorageType.SHORTLINK));
+  INITIALIZED = true;
 }
 
 async function getStorage(key: string, type: StorageType): Promise<any> {
-  const _key = storageKey(key, type);
-  const result = await chrome.storage.local.get(_key);
-  return result[_key];
+  if (!INITIALIZED) {
+    await initStorage();
+  }
+  switch (type) {
+    case StorageType.SHORTLINK:
+      return SHORTLINK_IDS[key];
+    case StorageType.ID_RESERVED:
+      return USED_IDS[key];
+  }
 }
 
-async function setStorage(
+function saveBlock(type: StorageType){
+  let block;
+  switch (type) {
+    case StorageType.SHORTLINK:
+      block = SHORTLINK_IDS;
+    case StorageType.ID_RESERVED:
+      block = USED_IDS;
+  }
+  chrome.storage.local.set({[storageKey(type)]: block }, () => {});
+}
+
+function setStorage(
   key: string,
   value: number | boolean,
   type: StorageType
 ) {
-  chrome.storage.local.set({ [storageKey(key, type)]: value }, () => {});
+  // write locally
+  switch (type) {
+    case StorageType.SHORTLINK:
+      SHORTLINK_IDS[key] = value;
+    case StorageType.ID_RESERVED:
+      USED_IDS[key] = value;
+  }
+
+  // save to disk
+  saveBlock(type);
 }
 
-async function removeStorage(key: string, type: StorageType) {
-  chrome.storage.local.remove([storageKey(key, type)]);
+function removeStorage(key: string, type: StorageType) {
+  // write locally
+  switch (type) {
+    case StorageType.SHORTLINK:
+      delete SHORTLINK_IDS[key];
+    case StorageType.ID_RESERVED:
+      delete USED_IDS[key];
+  }
+
+  // save to disk
+  saveBlock(type);
 }
 
-async function reserveID(id: number) {
+function reserveID(id: number) {
   setStorage(id.toString(), true, StorageType.ID_RESERVED);
 }
 
-async function freeID(id: number) {
+function freeID(id: number) {
   setStorage(id.toString(), false, StorageType.ID_RESERVED);
 }
 
@@ -86,12 +125,14 @@ async function getFreeID(): Promise<number | undefined> {
   // The iteration is capped so it shouldn't be a huge hit to speed.
   let id;
   // ID numbers must start at 1 to abide by chrome API rules
-  for (let i = 1; i <= MAX_LINKS; i++) {
+  let i = 1;
+  while ( i <= MAX_LINKS) {
     const isFree = await idIsFree(i);
     if (isFree) {
       id = i;
       break;
     }
+    i += 1;
   }
   return id;
 }
@@ -102,7 +143,7 @@ async function setShortLinkID(shortLink: string): Promise<number | undefined> {
     return;
   }
   reserveID(id);
-  setStorage(shortLink, id, StorageType.SHORTLINK);
+  await setStorage(shortLink, id, StorageType.SHORTLINK);
   return id;
 }
 
